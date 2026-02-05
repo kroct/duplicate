6e2b826720e1c2192122a02fda037cd395f95f6e
kmfouad <kmfouadm@gmail.com>
2026-02-05 07:07:39 +0200

    duplicate.c: Check file size first before checking the checksum

--- duplicate.c
+++ duplicate.c
@@ -1,33 +1,19 @@
-//fast -lxxhash
-// duplicate -- find all duplicates in a directory
-// depends on xxhash and uthash
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <sys/stat.h>
 #include <xxhash.h>
 #include <uthash.h>
+#include <sys/stat.h>
 
 #define SMALL_HASH_BYTES 4096
 
 struct filehash {
-    XXH64_hash_t digest;
-    char *path;
-    UT_hash_handle hh;
+   size_t size;
+   char *path;
+   UT_hash_handle hh;
 };
 
 struct filehash *filehashes = NULL;
-
-#undef HASH_FUNCTION
-#undef HASH_KEYCMP
-#define HASH_FUNCTION(s,len,hashv) ((hashv) = *s)
-#define HASH_KEYCMP(a,b,len) (*(XXH64_hash_t*)a != *(XXH64_hash_t*)b)
-
-static void
-die(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
-    exit(1);
-}
 
 static inline size_t
 filesize(const char *path) {
@@ -38,88 +24,93 @@ filesize(const char *path) {
 
 static XXH64_hash_t
 checksum(const char *path, int nbytes) {
-    size_t size;
-    XXH64_hash_t hash;
-    char *buffer;
-    FILE *file;
+   size_t size;
+   XXH64_hash_t hash;
+   char *buffer;
+   FILE *file;
 
-    fflush(stdout);
-    file = fopen(path, "r");
-    if(!file)
-        die("Failed to open file");
-    if(nbytes > 0) {
-        size = nbytes;
-    } else {
-        fseek(file, 0, SEEK_END);
-        size = ftell(file);
-        fseek(file, 0, SEEK_SET);
-    }
-    buffer = malloc(size+1);
-    if(!buffer)
-        die("Out of memory");
-    size = fread(buffer, 1, size, file);
-    hash = XXH64(buffer, size, 0);
-    free(buffer);
-    fclose(file);
-    return hash;
+   fflush(stdout);
+   file = fopen(path, "r");
+   if(!file) {
+      fprintf(stderr, "%s: No such file or directory\n", path);
+      exit(1);
+   }
+   if(nbytes > 0) {
+      size = nbytes;
+   } else {
+      fseek(file, 0, SEEK_END);
+      size = ftell(file);
+      fseek(file, 0, SEEK_SET);
+   }
+   buffer = malloc(size+1);
+   if(!buffer) {
+      fprintf(stderr, "Out of memory\n");
+      exit(1);
+   }
+   size = fread(buffer, 1, size, file);
+   hash = XXH64(buffer, size, 0);
+   free(buffer);
+   fclose(file);
+   return hash;
 }
 
 static void
 process_dir(const char *parent_path) {
-    DIR *dir;
-    static struct dirent *d;
+   DIR *dir;
+   static struct dirent *d;
 
-    dir = opendir(parent_path);
-    if(!dir) {
-        fprintf(stderr, "Couldn't open %s/\n", parent_path);
-        return;
-    }
-    while((d = readdir(dir))) {
-        char path[PATH_MAX];
+   dir = opendir(parent_path);
+   if(!dir) {
+      fprintf(stderr, "%s: Couldn't open directory\n", parent_path);
+      return;
+   }
+   while((d = readdir(dir))) {
+      char path[PATH_MAX];
+      if(d->d_name[0] == '.')
+         continue;
+      snprintf(path, sizeof(path), "%s/%s", parent_path, d->d_name);
+      if(d->d_type == DT_DIR) {
+         process_dir(path);
+      } else {
+         struct filehash *f, *suspect;
+         size_t size;
 
-        if(d->d_name[0] == '.')
+         size = filesize(path);
+         if(!size)
             continue;
 
-        snprintf(path, sizeof(path), "%s/%s", parent_path, d->d_name);
+         f = malloc(sizeof(struct filehash));
+         if(!f) {
+            fprintf(stderr, "Out of memory\n");
+            exit(1);
+         }
+         f->size = size;
 
-        if(d->d_type == DT_DIR) {
-            process_dir(path);
-        } else {
-            struct filehash *f, *suspect;
-            f = malloc(sizeof(struct filehash));
-            if(!f)
-                die("Out of memory");
-            f->digest = checksum(path, SMALL_HASH_BYTES);
-            if(!f->digest) {
-                free(f);
-                continue;
+         f->path = strdup(path);
+         HASH_FIND(hh,filehashes,&f->size,sizeof(size_t),suspect);
+         if(!suspect) {
+            HASH_ADD(hh,filehashes,size,sizeof(size_t),f);
+         } else {
+            if(checksum(path, SMALL_HASH_BYTES) == checksum(suspect->path, SMALL_HASH_BYTES)
+               && checksum(path, 0) == checksum(suspect->path, 0)) {
+                  printf("rm %s # %s\n", path, suspect->path);
+                  continue;
             }
-            f->path = strdup(path);
-            HASH_FIND(hh,filehashes,&f->digest,sizeof(XXH64_hash_t),suspect);
-            if(!suspect) {
-                HASH_ADD(hh,filehashes,digest,sizeof(XXH64_hash_t),f);
-            } else if(filesize(path) == filesize(suspect->path)) {
-                XXH64_hash_t c1 = checksum(path, 0);
-                XXH64_hash_t c2 = checksum(suspect->path, 0);
-                if(c1 == c2) {
-                    printf("rm %s # %s\n", path, suspect->path);
-                    continue;
-                }
-            }
-        }
-    }
-    closedir(dir);
+         }
+      }
+   }
+   closedir(dir);
 }
 
 int
 main(int argc, char **argv) {
-    if(argc < 2)
-        return 1;
-    for(int i = 1; i < argc; i++) {
-        size_t len;
-        len = strlen(argv[i]);
-        while(argv[i][--len] == '/')
-            argv[i][len] = '\0';
-        process_dir(argv[i]);
-    }
+   if(argc < 2)
+      return 1;
+   for(int i = 1; i < argc; i++) {
+      size_t len;
+      len = strlen(argv[i]);
+      while(argv[i][--len] == '/')
+         argv[i][len] = '\0';
+      process_dir(argv[i]);
+   }
 }
